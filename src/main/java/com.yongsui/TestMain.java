dafb11cde7429eb47ef9a7544709b72b91335783
linbet <2436791899@qq.com>
2021-10-21 21:11:04 +0800

    冲突

--- src/main/java/com.yongsui/TestMain.java
+++ src/main/java/com.yongsui/TestMain.java
@@ -2,7 +2,6 @@ package com.yongsui;
 
 public class TestMain {
     public static void main(String[] args) {
-        System.out.println("只提交不推送");
-        System.out.println("这是one11分支");
+        int a = 10;
     }
 }
